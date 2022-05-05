2d5d547232435d559a05afd0258fcff905fe3d6d
Mário Ferreira <a40250@alunos.ipb.pt>
2022-05-05 17:54:21 +0100

    5.Make a Vector

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Vector3 m_Movement;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-   // Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis ("Horizontal");
